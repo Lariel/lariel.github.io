b3b387917c77dc4a6c040f7dea6981475b29e1e5
Lariel N. Santos <lariel.negreiros@gmail.com>
2018-03-07 01:23:30 -0300

    testando possibilidades

--- js/application.js
+++ js/application.js
@@ -1,20 +1,11 @@
-//window.onload = function(){ SEM JQUERY
-	// alert('Olá!');
-//	var number = 2+2;
-//	console.log('vou escrever qlqr coisa aqui '+number);
-	// console.log(document.getElementById('first_p'));
-//	document.getElementById('first_p').className = 'blueColor'
-//}
-
 $(function(){  //Com JQUERY = jQuery()
-	alert('Olá!');
+	alert('Olá! Bem vindo a minha página de teste');
 	console.log('vou escrever qlqr coisa aqui ');
 
 	$('#changeBlueLink').click(changeColor);
 });
 
 function changeColor(){
-	//document.getElementById('first_p').className = 'blueColor'
 	var color_hexa = $('#color').val();
 	$('#first_p').css('color','#'+color_hexa);
 }
