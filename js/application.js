06d6553355bcdf678e6395efb573199bf766c5e0
10087134 <10087134@pucrs.br>
2020-08-06 17:06:52 -0300

    animação mensagem rodapé

--- js/application.js
+++ js/application.js
@@ -1,5 +1,28 @@
-$(function(){  //Com JQUERY = jQuery()
-	console.log('vou escrever qlqr coisa aqui .o/ ');
+(() => {
+	var typing = document.getElementById('typing'),
+		text = typing.textContent,
+		typed = typing.childNodes[0],
+		cursor = document.createElement('span')
 
-});
+	typed.textContent = '';
+	typing.appendChild(cursor).textContent = '_';
+	// Delay para inicializar a animação
+	setTimeout(() => {
+		requestAnimationFrame(type);
+	}, 1400)		
+	
+	function type() {
+		typed.textContent = text.substr(0, typed.textContent.length + 1);
+		
+		// Intervalo entre a digitação de cada caractere
+		setTimeout(() => {
+			requestAnimationFrame(type);
+		}, 150);
+	}
+			
+	// Intervalo do cursor ao final da animação
+	setInterval(() => {
+		cursor.style.visibility = cursor.style.visibility ? '' : 'hidden';
+	}, 500);
+})();
 
